c7f09e0fa345693f3f3ef0542bb2a9d63a1c0f26
FarahnazIZ <120684814+FarahnazIZ@users.noreply.github.com>
2023-03-01 22:18:43 +0330

    Upadate ForecastInformation component

--- src/ForecastInformation.js
+++ src/ForecastInformation.js
@@ -2,17 +2,30 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function ForecastInformation(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}°`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temperature.minimum);
+    return `${temperature}°`;
+  }
+
+  function callDay() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDate();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+
   return (
     <div className="ForecastInformation">
-      <div>{props.data.time}</div>
+      <div>{callDay()}</div>
       <WeatherIcon code={props.data.condition.icon} size={32} />
       <div className="mt-3">
-        <span className="max-temperature">
-          {props.data.temperature.maximum}°
-        </span>
-        <span className="min-temperature">
-          {props.data.temperature.minimum}°
-        </span>
+        <span className="max-temperature">{maxTemperature()}</span>
+        <span className="min-temperature">{minTemperature()}</span>
       </div>
     </div>
   );
