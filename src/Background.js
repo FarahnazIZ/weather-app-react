56ed40e0627106139682656716a1b8e34c1f62de
FarahnazIZ <120684814+FarahnazIZ@users.noreply.github.com>
2023-03-04 20:23:24 +0330

    Added index.js in img folder

--- src/Background.js
+++ src/Background.js
@@ -1,11 +1,11 @@
 import React from "react";
-import image from "./img/cloudy.jpg";
+import Images from "./img/index";
 
 export default function Background(props) {
   return (
     <div className="Background">
       <div className="overlay"></div>
-      <img src={image} alt="" />
+      <img src={Images.brokenCloudsD} alt="" />
     </div>
   );
 }
