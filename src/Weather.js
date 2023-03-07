5199494dad6e1f48ad5ac4225e3460e856adecce
FarahnazIZ <120684814+FarahnazIZ@users.noreply.github.com>
2023-03-07 23:45:29 +0330

    Updated loader spinner

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,7 @@ import WeatherInformation from "./WeatherInformation";
 import Forecast from "./Forecast";
 import "./Weather.css";
 import Background from "./Background";
+import { RotatingLines } from "react-loader-spinner";
 
 export default function Weather(props) {
   const [city, setCity] = useState(props.defaultCity);
@@ -72,8 +73,14 @@ export default function Weather(props) {
     search();
 
     return (
-      <div className="d-flex justify-content-center">
-        <div className="spinner-border mt-5" role="status"></div>
+      <div className="spinner">
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="96"
+          visible={true}
+        />
       </div>
     );
   }
